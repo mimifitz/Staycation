b6da684063df894061c7ee93f415be0ad083de1d
Mimi <mimi@MacBook-Air-Oj-11.local>
2020-04-11 16:42:43 +0200

    troubleshooting still

--- model/database.js
+++ model/database.js
@@ -19,28 +19,25 @@ con.connect(function (err) {
     console.log("Connected!");
 
     //edit this to create database tables
-<<<<<<< HEAD
     let sql = '';
-=======
-    let sql =
->>>>>>> 102612abe603570bb5781a866f5e4e15fed0c890
+
     // Here we are making sure to wipe out any tables that already exist!
     'SET FOREIGN_KEY_CHECKS = 0; ' +
 
         'DROP TABLE if exists locations; DROP TABLE if exists users; DROP TABLE if exists listings; DROP TABLE if exists bookings; DROP TABLE if exists reviews; ' +
         // Here we actually create the tables
 
-        // "CREATE TABLE users ( id integer PRIMARY KEY, email varchar(255) UNIQUE NOT NULL, phone_number integer UNIQUE, num_country_code integer, name varchar(255), preferred_contact_method varchar(255) ); " +
-        // "CREATE TABLE listings ( id integer PRIMARY KEY, user_id integer, date_published date, space_type varchar(255), is_shared boolean, location_id integer, reviews_id integer ); " +
-        // "CREATE TABLE locations ( id integer PRIMARY KEY, street_name varchar(255), city_name varchar(255), latitude float, longitude float ); " +
-        // "CREATE TABLE bookings ( id integer PRIMARY KEY, user_id integer, listing_id integer, start_date date, end_date date ); " +
-        // "CREATE TABLE reviews ( id integer PRIMARY KEY, booking_id integer, rating tinyint, review_body text ); " +
-        // // Here we enforce referencial integrity through foreign keys
-        // "ALTER TABLE listings ADD FOREIGN KEY (user_id) REFERENCES users (id); " +
-        // "ALTER TABLE listings ADD FOREIGN KEY (reviews_id) REFERENCES reviews (id); " +
-        // "ALTER TABLE bookings ADD FOREIGN KEY (user_id) REFERENCES users (id); " +
-        // "ALTER TABLE bookings ADD FOREIGN KEY (listing_id) REFERENCES listings (id); " +
-        // "ALTER TABLE reviews ADD FOREIGN KEY (booking_id) REFERENCES bookings (id); " +
+        "CREATE TABLE users ( id integer PRIMARY KEY, email varchar(255) UNIQUE NOT NULL, phone_number integer UNIQUE, num_country_code integer, name varchar(255), preferred_contact_method varchar(255) ); " +
+        "CREATE TABLE listings ( id integer PRIMARY KEY, user_id integer, date_published date, space_type varchar(255), is_shared boolean, location_id integer, reviews_id integer ); " +
+        "CREATE TABLE locations ( id integer PRIMARY KEY, street_name varchar(255), city_name varchar(255), latitude float, longitude float ); " +
+        "CREATE TABLE bookings ( id integer PRIMARY KEY, user_id integer, listing_id integer, start_date date, end_date date ); " +
+        "CREATE TABLE reviews ( id integer PRIMARY KEY, booking_id integer, rating tinyint, review_body text ); " +
+        // Here we enforce referencial integrity through foreign keys
+        "ALTER TABLE listings ADD FOREIGN KEY (user_id) REFERENCES users (id); " +
+        "ALTER TABLE listings ADD FOREIGN KEY (reviews_id) REFERENCES reviews (id); " +
+        "ALTER TABLE bookings ADD FOREIGN KEY (user_id) REFERENCES users (id); " +
+        "ALTER TABLE bookings ADD FOREIGN KEY (listing_id) REFERENCES listings (id); " +
+        "ALTER TABLE reviews ADD FOREIGN KEY (booking_id) REFERENCES bookings (id); " +
         'SET FOREIGN_KEY_CHECKS = 1; '
         ;
 
